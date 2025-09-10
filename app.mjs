fd471618703f482926e958c943a4807a0d4ccb0c
Elyas M. Kosari <elyas.kosari@gmail.com>
2025-09-10 10:57:23 -0400

    Initial Commit

---
+++ app.mjs
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+
+export class OrgCreator {
+  credGenerator = (start, end, groupName) => {
+    const groupId = uuidv4();
+    const credentials = [];
+    for (let i = start; i <= end; i++) {
+      credentials.push({
+        Name: `ARC101-${i}`,
+        SfName: `admin@arc101.${i}.com`,
+        Password: "password@1234",
+        GroupId: groupId,
+        Type: {
+          Id: "Production",
+          Domain: "https://login.salesforce.com/",
+          LP: "HOME",
+          landingPageOtherUrl: "",
+        },
+      });
+    }
+    return {
+      groups: [
+        {
+          Name: groupName,
+          isOpen: true,
+          credentials: credentials,
+        },
+      ],
+    };
+  };
+}
+
+const orgs = new OrgCreator();
+const result = orgs.credGenerator(15780, 15802, "ARC101 Sep. 8th");
+fs.writeFileSync("output.json", JSON.stringify(result, null, 2), "utf-8");
